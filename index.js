7f91d27fb87b6a26f458dfd5c81f5deef970e52f
unknown <kc9gpj12@gmail.com>
2018-08-29 20:32:04 -0500

    none

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-
-    // $("#brentIMG").animate({
-    //     height: '+=150px',
-    //     width: '+=150px'
-    // });
-    
-  });
-
-
+  var slideIndex = 1;
+  showDivs(slideIndex);
+  
+  function plusDivs(n) {
+      showDivs(slideIndex += n);
+  }
+  
+  function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      if (n > x.length) {slideIndex = 1} 
+      if (n < 1) {slideIndex = x.length} ;
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none"; 
+      }
+      x[slideIndex-1].style.display = "block"; 
+  }
 
